58cfca7602a40f67db4a6e11891e25fd1545c6e4
Sara Price <sbp354@log-1.hpc.nyu.edu>
2022-04-20 02:12:03 -0400

    cleaning up and adding new shell scripts for running eval versions of models

---
+++ shell_scripts/run_civil_eval_bert.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+export TOXIC_DIR=/scratch/sbp354/DSGA1012/Final_Project/data
+export TASK_NAME=Toxic
+export TRAIN_DATASET=civil_comments_0.5
+export DEV_DATASET=covert_comments
+
+export DATA=$1
+export RAN=$2
+export MODEL_DIR=/scratch/sbp354/DSGA1012/Final_Project/models/civil_comments_0.5
+
+python ../DSGA1012/Final_Project/git/Toxic_Debias/run_toxic.py \
+  --model_type bert \
+  --model_name_or_path bert-base-uncased \
+  --task_name $TASK_NAME \
+  --no-do_train \
+  --do_eval \
+  --evaluate_during_training \
+  --save_steps 1000 \
+  --logging_steps 1000 \
+  --overwrite_output_dir \
+  --data_dir $TOXIC_DIR \
+  --train_dataset $TRAIN_DATASET \
+  --dev_dataset $DEV_DATASET \
+  --max_seq_length 128 \
+  --per_gpu_train_batch_size 8 \
+  --per_gpu_eval_batch_size 8 \
+  --learning_rate 1e-5 \
+  --num_train_epochs 3.0 \
+  --output_dir $MODEL_DIR
