17a6a15ad35233fd9c80017afc80c813edf51a46
Pavel Gladkevich <pgladkevich@gmail.com>
2022-04-27 23:32:36 -0400

    Shallow founta creation

---
+++ shell_scripts/create_founta_shallow.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export TOXIC_DIR=/scratch/sbp354/DSGA1012/Final_Project/data
+export TRAIN_DATASET=founta_train.csv
+export MODE=random_005
+
+python /scratch/pg2255/nlu/Toxic_Debias/src/shallow_subsample.py \
+  --overwrite_output_dir \
+  --data_dir $TOXIC_DIR \
+  --train_dataset $TRAIN_DATASET \
+  --output_dir $TOXIC_DIR
+  --mode $MODE
+  
