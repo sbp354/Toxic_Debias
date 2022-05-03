5b953e57eee864ce3d33f141c81d4c3a9dfc57a9
Pavel Gladkevich <pg2255@log-3.hpc.nyu.edu>
2022-05-02 20:14:04 -0400

    Changed directories back to my scratch

--- shell_scripts/run_debias_founta_eval_covert.sh
+++ shell_scripts/run_debias_founta_eval_covert.sh
@@ -7,9 +7,9 @@ export TEACHER_DATASET=finetune_founta/founta_train_shallow_remainder_random_0.0
 export DEV_DATASET=covert_comments
 export LOSS=bias_product_by_teacher
 
-export MODEL_DIR=/scratch/dmm9812/NLU/models/founta
+export MODEL_DIR=/scratch/pg2255/nlu/run_results/models/founta
 
-python /scratch/dmm9812/Toxic_Debias/run_toxic.py \
+python /scratch/pg2255/nlu/Toxic_Debias/run_toxic.py \
   --model_type roberta \
   --model_name_or_path roberta-large \
   --task_name $TASK_NAME \
@@ -29,6 +29,6 @@ python /scratch/dmm9812/Toxic_Debias/run_toxic.py \
   --per_gpu_train_batch_size 8 \
   --per_gpu_eval_batch_size 8 \
   --learning_rate 1e-5 \
-  --num_train_epochs 5.0 \
+  --num_train_epochs 3.0 \
   --output_dir $MODEL_DIR \
   --mode $LOSS
