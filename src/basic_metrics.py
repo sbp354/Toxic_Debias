62a069bf736012d94bb2709fe9f6219d75a34660
Pavel Gladkevich <pgladkevich@gmail.com>
2022-05-08 14:27:16 -0400

    Modifying to save to results directory and have dynamic naming

--- src/basic_metrics.py
+++ src/basic_metrics.py
@@ -27,12 +27,14 @@ def get_scores(df, label_name='true_labels', pred_name='predictions', score_name
         recall_pos = recall_score(labels, predictions) #Not sure about how to use the threshold for these for now
         #recall_neg = recall_score(labels, predictions, pos_label=0) # Not sure about this 
         
+    predicted_prevalence = predictions.mean()
     avg_scores = scores.mean()
     f1 = f1_score(y_true = labels, y_pred = predictions, average=f1_avg)
     
     auc_roc = roc_auc_score(labels, scores)
     
     metrics  = {'avg-scores': avg_scores,
+                'predicted-prevalence': predicted_prevalence,
                 'f1': f1,
                 'auc-roc': auc_roc,
                 'fpr': recall_pos,  # FPR
@@ -75,16 +77,57 @@ def main():
                     help = "The column name for the model predictions",
                     required=False,
                     default="scores")
+
+    parser.add_argument(
+        "--pAPI",
+        default=None,
+        type=str,
+        required=False,
+        help="If pAPI create predictions column based off of toxicity score",
+    )
+
+    parser.add_argument("--output_dir",
+                    default="/scratch/sbp354/DSGA1012/Final_Project/models/results",
+                    type=str,
+                    required=False,
+                    help="metrics out put file name")
+
+    parser.add_argument("--output_name",
+                    default=None,
+                    type=str,
+                    required=False,
+                    help="Metrics out put file name")
     
     args = parser.parse_args()
 
 
     df = pd.read_csv(os.path.join(args.data_dir, args.results_csv))
     metrics = get_scores(df, args.label_name, args.pred_name, args.score_name)
-    file_name = args.results_csv[:-4] + '_basic_metrics.txt'
-    with open(os.path.join(args.data_dir, file_name), 'w') as f: 
-        for key, value in metrics.items(): 
-            f.write('%s:%s\n' % (key, value))
+
+    if args.output_name:
+        output_path =  os.path.join(args.output_dir,args.output_name)
+    else:
+        if args.pAPI:
+            output_name = args.results_csv[:-4] + '_basic_metrics.txt'
+        else: 
+            datasets = ['founta','civil_comments','civil_comments_0.5']
+            struct = ' '.join(args.model_dir.split('/')).split()  # This makes sure if there is / at the end its fine
+            model = struct[-2]
+            loss = struct[-1]
+            if model in datasets:
+                output_name = loss + args.results_csv[:-4]  # If no custom loss function then model name is here
+            else:
+                output_name = model + loss + args.results_csv[:-4]
+
+        output_path = os.path.join(args.output_dir,output_name)
+
+
+    # with open(os.path.join(args.data_dir, file_name), 'w') as f: 
+    #     for key, value in metrics.items(): 
+    #         f.write('%s:%s\n' % (key, value))
+    metrics_df = pd.DataFrame(metrics)
+    print(metrics_df)
+    metrics_df.to_csv(output_path)
 
 
 if __name__ == "__main__":
